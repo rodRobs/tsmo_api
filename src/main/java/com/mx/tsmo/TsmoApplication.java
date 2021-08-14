a55aa3f3577bc0e0512cbb0afcb77e825ffdfc1e
Joshue Zavaleta <Joshue@iMac-de-Joshue.local>
2021-08-14 12:30:51 -0500

    Ruta imagen en docuemntos jrxml

--- src/main/java/com/mx/tsmo/TsmoApplication.java
+++ src/main/java/com/mx/tsmo/TsmoApplication.java
@@ -2,10 +2,10 @@ package com.mx.tsmo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TsmoApplication extends SpringBootServletInitializer {
+public class TsmoApplication/* extends SpringBootServletInitializer */{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TsmoApplication.class, args);
